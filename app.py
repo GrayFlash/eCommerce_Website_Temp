596af5904705259335d6d21d09691a77d4facc63
GrayFlash <gk74533@gmail.com>
2021-05-02 13:37:26 +0530

    Homepage Responsive

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import numpy as np
 
+noOfItems = 0
+
 categories = ["Clothing_and_Accessories", "Software_Apparels"]
 subCategories = {"Clothing_and_Accessories":["Mens_Accessories", "Mens_Clothing", "Kids_Clothing", "Womens_Accessories", "Womens_Bags"]
 }
 subCategoryData = {}
-
+# category_img = ["images/clothing_accesories.jpg", "images/software_devices.jpg"]
+category_img = ["https://www.shutterstock.com/image-vector/fashion-store-boutique-accessories-bags-footwear-786450895", "https://www.shutterstock.com/image-photo/business-devices-on-brown-wooden-desk-1373777756"]
 
 for data in subCategories["Clothing_and_Accessories"]:
     df = pd.read_csv(data+".csv")
@@ -32,4 +35,9 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def Catalogue():
-    return render_template("index.html")
+    pass_data = []
+    for i in range(len(categories)):
+        name = categories[i]
+        link = category_img[i]
+        pass_data.append([name, link])
+    return render_template("index.html", pass_data=pass_data, noOfItems=noOfItems)
